a928df5273471d2045e71cbeae4ea67cd879ee18
Suchan An <dobestan@gmail.com>
2023-01-22 15:36:30 +0900

    Update POC Contract: add Ownable access control

--- contracts/POC.sol
+++ contracts/POC.sol
@@ -4,9 +4,15 @@ pragma solidity >= 0.8.0;
 
 import "./extensions/ERC20Epochs.sol";
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract POC is ERC20Epochs {
-    constructor() ERC20Epochs("POC", "POC") {
+
+contract POC is ERC20Epochs, Ownable {
+    constructor() ERC20Epochs("Decipher POC", "POC") {
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
     }
 
     // Examples for POC(Proof of Contribution) specific logics.
